dfc8458d32352d15cf5c0ec810328fab69da4cb7
HirGior <dimasamots@yandex.ru>
2026-03-29 14:49:22 +0300

    LAB1 compl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include "iostream"
 
 int main() {
+    const char* str = "Hello world test123 ABC test";
+
+    std::cout << "faStr1: " << faStr1(str) << std::endl;
+    std::cout << "faStr2: " << faStr2(str) << std::endl;
+    std::cout << "faStr3: " << faStr3(str) << std::endl;
+    
     return 0;
 }
